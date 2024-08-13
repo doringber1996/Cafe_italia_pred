6b985b4305b746894ed70754d08555547b658c66
Dor Ingber <82899447+doringber1996@users.noreply.github.com>
2024-08-14 00:50:49 +0300

    Update app.py

--- app.py
+++ app.py
@@ -162,7 +162,6 @@ features_rf = [feature for feature in features if feature not in ['לקוחות 
 features_svr = features
 features_stacking_rf = features 
 
-# פונקציה להוספת פיצ'רים נגזרים לסט האימון
 def add_features(data, average_customers_per_day, average_customers_per_month, high_corr_pairs):
     data['תאריך'] = pd.to_datetime(data['תאריך'])
     data['יום בשבוע'] = data['תאריך'].dt.dayofweek + 1
@@ -197,10 +196,11 @@ def preprocess_input_svr(start_date, end_date, num_customers, average_customers_
     data = pd.DataFrame({'תאריך': dates})
     data['מספר לקוחות'] = num_customers
     data = add_features(data, average_customers_per_day, average_customers_per_month, high_corr_pairs)
-    scaler = MinMaxScaler()
-    data['מספר לקוחות מנורמל'] = scaler.fit_transform(data[['מספר לקוחות']])
-    return data
-
+    
+    scaler_X = MinMaxScaler()
+    data[features_svr] = scaler_X.fit_transform(data[features_svr])
+    
+    return data, scaler_X
 
 # Preprocessing function for RF and Stacking RF
 def preprocess_input_rf(start_date, end_date, num_customers, average_customers_per_day, average_customers_per_month, high_corr_pairs):
@@ -212,20 +212,20 @@ def preprocess_input_rf(start_date, end_date, num_customers, average_customers_p
 
 def predict_dishes(start_date, end_date, num_customers, average_customers_per_day, average_customers_per_month, high_corr_pairs):
     results = {}
-    input_data_svr = preprocess_input_svr(start_date, end_date, num_customers, average_customers_per_day, average_customers_per_month, high_corr_pairs)
+    input_data_svr, scaler_X = preprocess_input_svr(start_date, end_date, num_customers, average_customers_per_day, average_customers_per_month, high_corr_pairs)
     input_data_rf = preprocess_input_rf(start_date, end_date, num_customers, average_customers_per_day, average_customers_per_month, high_corr_pairs)
 
     for dish in dish_columns:
         best_model_type = optimal_models_df.loc[optimal_models_df['Dish'] == dish, 'Model'].values[0]
         if best_model_type == 'SVR':
-            predictions = load_model_and_predict(dish, input_data_svr, best_model_type)
+            predictions = load_model_and_predict(dish, input_data_svr, best_model_type, scaler_X)
         else:
             predictions = load_model_and_predict(dish, input_data_rf, best_model_type)
         results[dish] = predictions
 
     return results
     
-def load_model_and_predict(dish, input_data, model_type):
+def load_model_and_predict(dish, input_data, model_type, scaler_X=None):
     model_type = model_type.lower()
     if model_type == 'svr':
         model_file = f'{models_path}best_svr_model_{dish}.pkl'
@@ -251,13 +251,17 @@ def load_model_and_predict(dish, input_data, model_type):
         st.error(f"Error in loading the model: {model_file}, Error: {e}")
         return np.array([])
 
-    predictions = model.predict(features)
-    predictions = np.ceil(predictions).astype(int)
+    predictions_scaled = model.predict(features)
+    
+    if model_type == 'svr' and scaler_X is not None:
+        predictions = scaler_X.inverse_transform(predictions_scaled.reshape(-1, 1)).flatten()
+    else:
+        predictions = np.ceil(predictions_scaled).astype(int)
 
     return predictions
-    
+
 # Compute average customers per day and month
-average_customers_per_day= {1: 264.2421052631579, 2: 284.775, 3: 294.87704918032784, 4: 296.3606557377049, 5: 352.64516129032256, 6: 354.008064516129, 7: 357.3414634146341}
+average_customers_per_day = {1: 264.2421052631579, 2: 284.775, 3: 294.87704918032784, 4: 296.3606557377049, 5: 352.64516129032256, 6: 354.008064516129, 7: 357.3414634146341}
 average_customers_per_month = {1: 334.28409090909093, 2: 350.51851851851853, 3: 337.4623655913978, 4: 313.58024691358025, 5: 309.14606741573033, 6: 307.3448275862069, 7: 312.4561403508772, 8: 336.41379310344826, 9: 310.47058823529414, 10: 216.94545454545454, 11: 282.43859649122805, 12: 352.66129032258067}
 
 # Create a list to store pairs of highly correlated dishes
@@ -390,7 +394,6 @@ high_corr_pairs=[("חציל פרמז'ן", "פוקצ'ת הבית", 0.679420403402
  ('טירמיסו', 'פנה קרבונרה', 0.5576175435814857),
  ('טירמיסו', 'פילה דג', 0.5155064783394511),
  ('טירמיסו', 'פרגיות', 0.5306007273916324)]
-
 st.markdown(
     f"""
     <style>
@@ -477,3 +480,4 @@ if st.button("Predict"):
     st.markdown('<h2 class="title">Download Results</h2>', unsafe_allow_html=True)
     csv = predictions_df.to_csv(index=False)
     st.download_button(label="Download CSV", data=csv, file_name='predictions.csv', mime='text/csv')
+
